6ee5b99c875b4aedcdb2de2178997ab6542dd649
Shami Lakhani <Shami.Lakhani@homeretailgroup.com>
2019-02-04 23:00:48 +0000

    inital build

--- go.py
+++ go.py
@@ -7,16 +7,24 @@ from cachetools import cached, TTLCache
 cache_validateDomain = TTLCache(maxsize=8000, ttl=2000)
 cache_suggestDomain = TTLCache(maxsize=8000, ttl=2000) # set a lower ttl for new domains added to the safelist during i_validateDomain_MX to come into affect
 app = flask.Flask(__name__)
-safeList={'gmail.com','hotmail.com','homeretailgroup.com','cscglobal.com'}
+safeList={'gmail.com'}
 
-app.config["DEBUG"] = True
+app.config["DEBUG"] = False
+
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 @cached(cache_validateDomain)
 def i_validateDomain_MX(domain):  # function performing under cache to reduce the number of calls
     try:
-        query(domain, 'MX')
-        safeList.add (domain) # add domain with valid MX record to the safelist - will be used once the suggestCorrectDomain cache expires
-        return True # return positive as domain has a valid MX record
+        if domain in safeList:
+            print ("safelist")
+            return True
+        else:
+            query(domain, 'MX')
+            safeList.add (domain) # add domain with valid MX record to the safelist - will be used once the suggestCorrectDomain cache expires
+            return True # return positive as domain has a valid MX record
     except:
         return False # return negative as domain has no valid MX record
 
@@ -38,7 +46,7 @@ def home():
 def validateDomain():
     if 'domain' in request.args:
         ext_domainToValidate = str(request.args['domain'])
-        results = i_validateDomain_MX(ext_domainToValidate)
+        results = int(i_validateDomain_MX(ext_domainToValidate))
     else:
         return "Error: No domain field provided. Please specify an domain."
     return jsonify(results)
@@ -50,6 +58,14 @@ def suggestCorrectDomain():
         results = (i_suggestCorrectDomain(ext_domainToQuery))
     else:
         return "Error: No domain field provided. Please specify a domain."
+    return jsonify(results)
+
+@app.route('/admin/safeListSize',methods=['GET'])
+def listSize():
+    if 'safelistsize' in request.args:
+        results = str(len(safeList))
+    else:
+        return "Error: no field provided."
     return jsonify(results)
 
 app.run()
